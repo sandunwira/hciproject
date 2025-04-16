b8043ab7118d3f357d67d7770c6106da92ec0c4b
ylp1455 <yasiruperera681@gmail.com>
2025-04-17 01:22:45 +0530

    Created the following pages:

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import HomePage from './pages/Home';
 import PrivacyPolicyPage from './pages/PrivacyPolicy';
+import TermsOfServicePage from './pages/TermsOfService';
+import CookiePolicyPage from './pages/CookiePolicy';
+import LicensesPage from './pages/Licenses';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="/terms" element={<TermsOfServicePage />} />
+          <Route path="/cookies" element={<CookiePolicyPage />} />
+          <Route path="/licenses" element={<LicensesPage />} />
         </Routes>
       </Router>
     </AuthProvider>
